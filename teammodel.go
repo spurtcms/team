c2673eb5c5d2119a536ecdfb8ed3fc2f9b890104
spurtcmsdeveloper-46 <piccotvm46@gmail.com>
2024-07-29 17:53:01 +0530

    changes in update user

--- teammodel.go
+++ teammodel.go
@@ -167,7 +167,7 @@ func (t TeamModel) UpdateUser(user *TblUser, DB *gorm.DB) (team TblUser, terr er
 
 	} else {
 
-		if err := query.UpdateColumns(map[string]interface{}{"first_name": user.FirstName, "last_name": user.LastName, "role_id": user.RoleId, "email": user.Email, "username": user.Username, "mobile_no": user.MobileNo, "is_active": user.IsActive, "modified_on": user.ModifiedOn, "modified_by": user.ModifiedBy, "profile_image": user.ProfileImage, "profile_image_path": user.ProfileImagePath, "data_access": user.DataAccess, "password": user.Password}).Error; err != nil {
+		if err := query.UpdateColumns(map[string]interface{}{"first_name": user.FirstName, "last_name": user.LastName, "role_id": user.RoleId, "email": user.Email, "username": user.Username, "mobile_no": user.MobileNo, "is_active": user.IsActive, "modified_on": user.ModifiedOn, "modified_by": user.ModifiedBy, "profile_image": user.ProfileImage, "profile_image_path": user.ProfileImagePath, "data_access": user.DataAccess, "password": user.Password, "storage_type": user.StorageType}).Error; err != nil {
 
 			return TblUser{}, err
 		}
